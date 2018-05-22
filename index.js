8e215b98ca035eb6139c716ec3d637352a037583
Harry Dibden <harry-dibden@hotmail.com>
2018-05-22 21:35:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function currentLine(katzDeliLine) {
     }
     return `The line is currently: ${newLine}`;
   } else {
-    return "The line is currently empty."
+    return "The line is currently empty.";
   }
 }
